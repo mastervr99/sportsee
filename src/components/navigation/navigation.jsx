17c5608a8dc2de0ad1e0b089cffb594913c89ec3
mastervr99 <web.dev418@gmail.com>
2024-09-05 19:55:10 +0200

    feat : component navbar

---
+++ src/components/navigation/navigation.jsx
@@ -0,0 +1,14 @@
+import './navigation.scss';
+
+function Navigation() {
+    return <div className='navbar'>
+                <ul className='navList'>
+                    <li>Accueil</li>
+                    <li>Profil</li>
+                    <li>Réglage</li>
+                    <li>Communauté</li>
+                </ul>
+            </div>
+}
+
+export default Navigation
