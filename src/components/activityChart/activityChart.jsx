8c3da551ab9e21c5f93df383b04f6aa03111e5cf
mastervr99 <web.dev418@gmail.com>
2024-09-24 16:51:15 +0200

    refactor : activityChart

--- src/components/activityChart/activityChart.jsx
+++ src/components/activityChart/activityChart.jsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react';
 import { BarChart, Bar, Rectangle, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 import { userActivityModel } from '../../services/models/userActivityModel';
 import { getUserActivity } from '../../services/userService';
+import ActivityTooltip from './activityTooltip';
 
 
 function ActivityChart({userId}){
@@ -30,11 +31,17 @@ function ActivityChart({userId}){
       return <div>Loading...</div>;
   }
 
+    const renderLegendText = (value, entry) => {
+      const { color } = entry;
+      return <span style={{ color: '#74798C' }}>{value}</span>;
+    };
+
     return <div className="activityChart">
         <ResponsiveContainer width="100%" height="100%" className="responsiveContainer">
         <BarChart
           width={500}
           height={300}
+          barGap={8}
           data={userDataArray}
           margin={{
             top: 30,
@@ -43,14 +50,15 @@ function ActivityChart({userId}){
             bottom: 30,
           }}
         >
-          <CartesianGrid strokeDasharray="1 2" vertical={false} stroke="#DEDEDE"/>
+          <CartesianGrid strokeDasharray="3" vertical={false} stroke="#DEDEDE"/>
           <text x={30} y={45} style={{fontSize: '16px', fontWeight: 'bold', fill: "#20253A"}}>
                 Activité quotidienne
             </text>
 
-          <XAxis dataKey="day" />
-          <YAxis  tickMargin={10} tick={{fill: "#9B9EAC", strokeWidth: 0.5}} orientation="right"  tickLine={false} stroke="transparent"/>
-          <Legend verticalAlign="top" iconType="circle" align="right" iconSize={8} height={80} payload={
+          <XAxis dataKey="day"  tickLine={false}  tick={{ fill: '#9B9EAC' }} tickMargin={16}/>
+          <YAxis yAxisId="kilogram" tickMargin={30} tick={{fill: "#9B9EAC", strokeWidth: 0.5}} orientation="right"  tickLine={false} stroke="transparent" domain={['dataMin-2', 'dataMax+1']}  tickCount={3}/>
+          <YAxis hide yAxisId="calories" />
+          <Legend className='activity_chart_legend' formatter={renderLegendText} verticalAlign="top" iconType="circle" align="right" iconSize={8} height={80} payload={
                         [{
                             value: 'Poids (kg)',
                             color: "#282D30",
@@ -59,9 +67,9 @@ function ActivityChart({userId}){
                             color: "#FF0101",
                         }
                         ]}/>
-          <Tooltip />
-          <Bar dataKey="kilogram" fill="#FFFFF" barSize={5} radius={[5, 5, 0, 0]} />
-          <Bar dataKey="calories" fill="#E60000" barSize={5} radius={[5, 5, 0, 0]} />
+          <Tooltip content={< ActivityTooltip active={false} payload={[]} />} />
+          <Bar yAxisId="kilogram" dataKey="kilogram" fill="#FFFFF" barSize={8} radius={[5, 5, 0, 0]} />
+          <Bar yAxisId="calories" dataKey="calories" fill="#E60000" barSize={8} radius={[5, 5, 0, 0]} />
         </BarChart>
       </ResponsiveContainer>
     </div>
